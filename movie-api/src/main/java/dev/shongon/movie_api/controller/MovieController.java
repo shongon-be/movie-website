cf4277046fbe0fbedb9919062230cbcdaa2c5675
Hong Son <hongson530.dev@gmail.com>
2025-06-28 21:27:09 +0700

    refactor: refactor un-used code

--- movie-api/src/main/java/dev/shongon/movie_api/controller/MovieController.java
+++ movie-api/src/main/java/dev/shongon/movie_api/controller/MovieController.java
@@ -5,7 +5,6 @@ import dev.shongon.movie_api.service.MovieService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
