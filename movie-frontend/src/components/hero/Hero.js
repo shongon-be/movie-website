af2438bf6c212fc574c46a0a09563b1aaeb54fc5
Hong Son <hongson530.dev@gmail.com>
2025-06-29 23:54:24 +0700

    refactor: missing typo

--- movie-frontend/src/components/hero/Hero.js
+++ movie-frontend/src/components/hero/Hero.js
@@ -22,7 +22,7 @@ const Hero = ({movies}) => {
                         return(
                             <Paper>
                                 <div className="movie-card-container">
-                                    <div className="movie-card" style={{"--img": `url(${movie.backdrop[0]})`}}>
+                                    <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
                                         <div className="movie-detail">
                                             <div className="movie-poster">
                                                 <img src={movie.poster} alt=""/>
